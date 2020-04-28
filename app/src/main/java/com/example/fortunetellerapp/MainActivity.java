63c47cb506ae938902abd331c00c439c5c829bf5
skodati <skodati2@illinois.edu>
2020-04-27 21:46:02 -0500

    1

--- app/src/main/java/com/example/fortunetellerapp/MainActivity.java
+++ app/src/main/java/com/example/fortunetellerapp/MainActivity.java
@@ -15,17 +15,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    final ImageView ballDisplay = (ImageView) findViewById(R.id.image_teller); // Grab the ballDisplay from the xml by the id
+    final ImageView girlDisplay = (ImageView) findViewById(R.id.imageView); // Grab the ballDisplay from the xml by the id
 
-    final int[] ballArray = { // The ballArray holds the 5 ball drawables
-            R.drawable.ball1, // ball1 sits at index 0
-            R.drawable.ball2, // ball2 sits at index 1
-            R.drawable.ball3, // ball3 sits at index 2
-            R.drawable.ball4, // ball4 sits at index 3
-            R.drawable.ball5, // ball5 sits at index 4
+    final int[] girlArray = { // The ballArray holds the 5 ball drawables
+            R.id.msg1, // ball1 sits at index 0
+            R.id.msg2, // ball2 sits at index 1
+            R.id.msg3, // ball3 sits at index 2
+            R.id.msg4, // ball4 sits at index 3
+            R.id.msg5, // ball5 sits at index 4
     };
 
-    Button myButton = (Button) findViewById(R.id.askButton); // Grab the myButton from the xml by the id
+    Button myButton = (Button) findViewById(R.id.button2); // Grab the myButton from the xml by the id
 
         myButton.setOnClickListener(new View.OnClickListener() {
         @Override
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
             int number = randomNumberGenerator.nextInt(5); // Create a variable that stores a random number between 0 and 4
 
-            ballDisplay.setImageResource(ballArray[number]); // Set the image of the ballDisplay to a random number in the ballArray
+            girlDisplay.setImageResource(girlArray[number]); // Set the image of the ballDisplay to a random number in the ballArray
         }
     });
 
